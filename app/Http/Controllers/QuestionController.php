07c5f9e077922a718eee10dbc2631f1e09081421
thegreated <edwardarilla@gmail.com>
2020-02-09 02:18:25 +0800

    add question add form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -30,6 +30,9 @@ class QuestionController extends Controller
     public function create()
     {
         //
+        $question = new Question();
+
+        return view('questions.create', compact('$question'));
     }
 
     /**
