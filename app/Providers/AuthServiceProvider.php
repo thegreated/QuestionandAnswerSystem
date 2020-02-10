0ecf02c73fc1904af602a4e81dfe31781607f441
thegreated <edwardarilla@gmail.com>
2020-02-10 21:50:12 +0800

    Imlement update answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Question;
+use App\Answer;
+use App\Policies\AnswerPolicy;
 use App\Policies\QuestionPolicy;
 
 
@@ -16,7 +18,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-       Question::class =>QuestionPolicy::class
+       Question::class =>QuestionPolicy::class,
+       Answer::class =>AnswerPolicy::class
     ];
 
     /**
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -8,6 +8,7 @@
                     </div>
                     <hr>
                     @include('layouts._messages')
+                    
                     @foreach ($answers as $answer)
                         <div class="media">
                             <div class="d-flex flex-column vote-controls">
@@ -24,8 +25,24 @@
                                 </a>
                             </div>
                             <div class="media-body">
-                                {!! $answer->body_html !!}
-                                <div class="float-right">
+                            {!! $answer->body_html !!}
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="ml-auto">
+                                        @can('update',$answer)
+                                             <a href="{{ route('questions.answer.edit', [$question->id,$answer->id]) }}"  class="btn btn-sm btn-outline-info"> Edit </a>
+                                        @endcan
+                                        @can('delete',$answer)
+                                            <form class="form-delete" method="post" action="{{ route('questions.answer.destroy',[$question->id,$answer->id]) }} " >
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure')">Delete</button>
+                                            </form>
+                                        @endcan
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                <div class="col-4">
                                     <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $answer->user->url }}" class="pr-2"> 
@@ -36,6 +53,10 @@
                                         </div>
                                     </div>
                                 </div>
+                                </div>
+                            </div>
+                               
+                            
                                 
                             </div>
                         </div>
