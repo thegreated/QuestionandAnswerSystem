1b24507f7207cb9748ae6608d51a9109abd4b110
thegreated <edwardarilla@gmail.com>
2020-02-10 22:36:00 +0800

    Update best_answer_id on question table when an answer removed using eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -31,7 +31,12 @@ class Answer extends Model
             
         });
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
 
     }
@@ -40,6 +45,10 @@ class Answer extends Model
         
         return $this->created_at->diffFOrHumans();
         
+    }
+
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
         
 }
