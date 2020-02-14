e84e06fc72e6ab0a10e1223c1bc959c0184d82be
thegreated <edwardarilla@gmail.com>
2020-02-15 03:40:18 +0800

    Secure system from xxs attact

--- app/Question.php
+++ app/Question.php
@@ -44,7 +44,7 @@ class Question extends Model
 
     public function getBodyHtmlAttribute(){
 
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
 
     public function answers(){
@@ -79,10 +79,21 @@ class Question extends Model
 
         return $this->favorites()->count();
     }
-    public function geVotesAtCountAttribute(){
+    public function getexcerptAttribute(){
 
-        return $this->favorites()->count();
+       return $this->excerpt(250);
     }
 
+    public function excerpt($lenght){
+
+        return str_limit(strip_tags($this->bodyHtml()), $lenght);
+    }
+
+    private function bodyHtml(){
+
+        return \Parsedown::instance()->text($this->body);
+
+    }
+ 
 
 }
